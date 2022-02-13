4743bdf3d2f69ecfedf568410af7eb98a966f076
zangzangs <kimjangh94@gmail.com>
2022-02-13 22:51:32 +0900

    Inital commit

---
+++ src/main/java/com/example/exceptionlab/ExceptionlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionlabApplication.class, args);
+    }
+
+}
