3676ee2d45bc5b2da54aaba6b515ae346d74f5fb
zangzangs <kimjangh94@gmail.com>
2022-02-13 22:51:17 +0900

    Inital commit

---
+++ src/test/java/com/example/exceptionlab/ExceptionlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptionlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
