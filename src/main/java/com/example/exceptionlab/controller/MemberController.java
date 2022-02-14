7ceb5496947ae5c3ef34a3db84adbd131ba69c70
zangzangs <kimjangh94@gmail.com>
2022-02-14 19:04:22 +0900

    feat: httpstatus 어노테이션 추가

--- src/main/java/com/example/exceptionlab/controller/MemberController.java
+++ src/main/java/com/example/exceptionlab/controller/MemberController.java
@@ -2,10 +2,8 @@ package com.example.exceptionlab.controller;
 
 import com.example.exceptionlab.dto.MemberDto;
 import com.example.exceptionlab.service.MemberService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/member")
@@ -18,6 +16,7 @@ public class MemberController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Object saveMember(@RequestBody MemberDto memberDto){
         return memberService.saveMember(memberDto);
     }
---
+++ src/main/java/com/example/exceptionlab/exception/CommonException.java
@@ -0,0 +1,2 @@
+package com.example.exceptionlab.exception;public class CommonException {
+}
